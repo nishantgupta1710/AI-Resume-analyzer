19840716562addc006747b8b91c17ee5cda8c3f1
Nishant Gupta <gnishant025@gmail.com>
2026-08-27 14:27:25 +0530

    Fix root url

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/',views.home,name= 'home'),
+    path('',views.home,name= 'home'),
     path('login/',views.login_page,name='login'),
     path('signup/',views.signup_page,name='signup'),
     path('dashboard/',views.dashboard,name='dashboard'),
